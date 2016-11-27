7948c81b430fbaf05297d4894e46e4e3b5651486
normoneil <normoneil@users.noreply.github.com>
2016-11-27 15:39:10 -0600

    Step 16 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "photos#index"
-  
+
+  # Routes for the User resource:
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+  #------------------------------
+
   # Routes for the Comment resource:
   # CREATE
   get "/comments/new", :controller => "comments", :action => "new"
